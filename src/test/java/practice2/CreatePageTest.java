1dbcc3a55db0471d4fa84353b6b0d8b7b0231041
sneha <sneha@Sneha>
2026-04-30 14:46:47 +0530

    new job4

---
+++ src/test/java/practice2/CreatePageTest.java
@@ -0,0 +1,26 @@
+package practice2;
+
+import org.testng.annotations.Test;
+
+public class CreatePageTest {
+
+		
+		@Test
+		public void CreatePage() {
+			
+			System.out.println("page created");
+			
+		}
+		
+		
+		@Test
+			public void CreatePageWithDate() {
+			System.out.println("Page with date created");
+				
+			
+		}
+	
+
+	}
+
+
